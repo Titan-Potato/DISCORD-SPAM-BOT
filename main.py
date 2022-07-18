099515b74424e3ac1c6e8154fc57f7ee8e3e3111
Titan-Potato <85183400+Titan-Potato@users.noreply.github.com>
2022-07-18 09:22:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 channelID = 998487725915254854
 headers = {
     "authorization":
-    "OTk4NDg4MjI4ODQxNjY4NjE4.GPLgVy.jBQsKNLBCx3zSq69xzjYxGhm449AUdpvFsAbqI"
+    "OTk4NDg4MjI4ODQxNjY4NjE4.G5D9HJ.X-DLCxk9wnzWW2lF3jQ81td9bmBWIVXtLkopic"
 }
 keep_alive()
 file = open("text.txt", "r")
